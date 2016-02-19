85e5b0892d0a439100cf465084fcba7dd3f46f62
ivanwangsa <ivan@wangsacl.com>
2016-02-20 02:32:22 +0800

    angular forms

--- conFusion/gulpfile.js
+++ conFusion/gulpfile.js
@@ -14,6 +14,9 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     del = require('del');
 
+var ngannotate = require('gulp-ng-annotate');
+
+
 gulp.task('jshint', function() {
   return gulp.src('app/scripts/**/*.js')
   .pipe(jshint())
@@ -31,10 +34,10 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/contactus.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
-        js: [uglify(),rev()]
+        js: [ngannotate(),uglify(),rev()]
       }))
       .pipe(gulp.dest('dist/'));
 });
@@ -76,7 +79,7 @@ gulp.task('browser-sync', ['default'], function () {
    browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
+         index: "contactus.html"
       }
    });
         // Watch any files in dist/, reload on change
