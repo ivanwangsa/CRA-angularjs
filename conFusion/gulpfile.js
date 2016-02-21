a3954525a78d5cc7c5a40b7ab933c4293d8897d6
ivanwangsa <ivan@wangsacl.com>
2016-02-22 00:29:06 +0800

    Completed Angular templates

--- conFusion/gulpfile.js
+++ conFusion/gulpfile.js
@@ -83,7 +83,7 @@ gulp.task('browser-sync', ['default'], function () {
    browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "contactus.html"
+         index: "index.html"
       }
    });
         // Watch any files in dist/, reload on change
